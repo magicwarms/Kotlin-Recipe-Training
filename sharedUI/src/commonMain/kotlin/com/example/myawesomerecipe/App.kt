70691f9852a70977fce36bda73108463220078c7
Andhana Utama <andhanautama@gmail.com>
2026-07-08 10:59:55 +0700

    feat: add some necessaries things

--- sharedUI/src/commonMain/kotlin/com/example/myawesomerecipe/App.kt
+++ sharedUI/src/commonMain/kotlin/com/example/myawesomerecipe/App.kt
@@ -3,9 +3,11 @@ package com.example.myawesomerecipe
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -23,26 +25,20 @@ import myawesomerecipe.sharedui.generated.resources.compose_multiplatform
 @Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
         Column(
             modifier = Modifier
                 .background(MaterialTheme.colorScheme.primaryContainer)
                 .safeContentPadding()
                 .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
         ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me hi!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
+            Text("No Recipe data synced yet!")
+            Button(
+                onClick = {},
+                modifier = Modifier.padding()
+            ) {
+                Text("Sync Now")
             }
         }
     }
